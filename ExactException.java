7a30e200449afabc1ae7fec0ea6124a068395c6a
rudo <rudra.mondal@gmail.com>
2023-02-23 14:32:56 +0530

    done

---
+++ ExactException.java
@@ -0,0 +1,10 @@
+class ExactException {
+        public static void main(string[] args) {
+        try{
+             system.out.println(4/10);
+             }
+             catch(ArithmeticException e){
+                 System.out.println("divide by 0");
+             }
+        }
+    }
